3aa672ccd69d904700f50c7cc3d5cd8864d59767
Rafael <132296553+R4f4ell@users.noreply.github.com>
2025-03-31 09:44:19 -0300

    Configurando o terminal para utilizar o sass
    Problemas com a conexão para a continuação do portifólio.

---
+++ assets/js/teste.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.getElementById("menu");
+    const nav = document.getElementById("nav");
+  
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("active");
+    });
+  });
+  
