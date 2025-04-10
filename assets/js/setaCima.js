8c300e8fa27c2028685568200ddb35deda07b994
Rafael <132296553+R4f4ell@users.noreply.github.com>
2025-04-10 17:40:01 -0300

    Identação e minimização de arquivos

--- assets/js/setaCima.js
+++ assets/js/setaCima.js
@@ -1,16 +1,16 @@
 window.addEventListener('scroll', () => {
-    const arrow = document.querySelector('.scroll-arrow-up');
-    if (window.scrollY > 600) {
-      arrow.classList.add('visible');
-    } else {
-      arrow.classList.remove('visible');
-    }
-  });
-  
-  // Rolagem suave ao topo
-  function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+  const arrow = document.querySelector('.scroll-arrow-up');
+  if (window.scrollY > 600) {
+    arrow.classList.add('visible');
+  } else {
+    arrow.classList.remove('visible');
   }
-  
-  // Expor função ao escopo global (para funcionar com onclick no HTML)
-  window.scrollToTop = scrollToTop;
+});
+
+// Rolagem suave ao topo
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+// Expor função ao escopo global (para funcionar com onclick no HTML)
+window.scrollToTop = scrollToTop;
